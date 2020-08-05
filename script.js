3c4912ae8f5e8a414e3ab909e783d2eb9f62d63c
SoloJette <solemansay@gmail.com>
2020-08-04 21:54:55 -0400

    added the frame

---
+++ script.js
@@ -0,0 +1,14 @@
+var container = document.querySelector(".container");
+var questions = []
+var hpTitle = document.querySelector(".hpTitle")
+var buttons = document.querySelector("buttons")
+var answer1 = document.querySelector("answer1")
+var answer2 = document.querySelector("answer2")
+var answer3 = document.querySelector("answer3")
+var answer4 = document.querySelector("answer4")
+var question = document.querySelector("questionText")
+
+var score = 75
+
+
+
